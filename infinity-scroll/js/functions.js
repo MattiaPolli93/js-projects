4db1d1aa08b4acd8e5356510d9b718aea66c638d
Mattia Polli <77399568+MattiaPolli93@users.noreply.github.com>
2021-07-26 17:43:58 +0200

    Implement fetch API request

--- infinity-scroll/js/functions.js
+++ infinity-scroll/js/functions.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// Getting photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        // catching error
+        alert("Error!!!");
+        console.log(error);
+    }
+}
--- infinity-scroll/js/script.js
+++ infinity-scroll/js/script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+// Unsplash API
+const count = 10;
+
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+// On load
+getPhotos();
