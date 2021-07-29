742dc5536f0af6746d949605a1a72f6e5b5b2688
Mattia Polli <77399568+MattiaPolli93@users.noreply.github.com>
2021-07-29 12:16:58 +0200

    Implement functionalities

--- picture-in-picture/js/functions.js
+++ picture-in-picture/js/functions.js
@@ -1,1 +1,15 @@
 "use strict";
+
+// Prompt to select media stream, pass to video element and then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        alert("Error!");
+        console.log(error);
+    }
+}
--- picture-in-picture/js/script.js
+++ picture-in-picture/js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+button.addEventListener("click", async () => {
+    // Disable button
+    button.disabled = true;
+    // Start pic-in-pic
+    await videoElement.requestPictureInPicture();
+    // Reset button
+    button.disabled = false;
+});
+
+// On load
+selectMediaStream();
