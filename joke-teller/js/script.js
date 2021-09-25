ba94e4f9b051dcac3e8f1ffb53c8780ce5cf4337
Mattia Polli <77399568+MattiaPolli93@users.noreply.github.com>
2021-09-25 14:53:04 +0200

    Implement functionalities

--- joke-teller/js/script.js
+++ joke-teller/js/script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const button = document.getElementById("btn");
+const audioElement = document.getElementById("audio");
+
+// Get Jokes from JokeAPI
+async function getJokes() {
+    let joke = "";
+    const apiUrl = "https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,racist,sexist";
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        // Two-part Joke
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else { // One-part Joke
+            joke = data.joke;
+        }
+        // Text-to-Speech
+        tellMe(joke);
+        // Disable Button
+        toggleButton();
+    } catch (error) {
+        console.log("Sorry", error);
+    }
+}
+
+// Event Listeners
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
